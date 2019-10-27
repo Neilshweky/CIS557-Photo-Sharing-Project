738d45c10bb2b60bff67ec9484164148b7fc829e
Neil Shweky <neilshweky@gmail.com>
2019-10-26 20:02:02 -0400

    loader now exits

--- api/loader.js
+++ api/loader.js
@@ -1,4 +1,5 @@
 const Schemas = require('./models/schemas');
+const mongoose = require('mongoose');
 
 var user = new Schemas.User({
     username: "neilshweky",
@@ -19,19 +20,27 @@ var post = new Schemas.Post({
     username: "neilshweky"
 })
 
-Schemas.User.deleteMany({}, err => {
-    console.log("Users removed")
-    user.save().then(() => console.log("User saved"))
-        .catch(err => console.log("There was an error", err))    
-    user2.save().then(() => console.log("User saved"))
-        .catch(err => console.log("There was an error", err))    
-    
-})
-Schemas.Post.deleteMany({}, err => {
-    console.log("Posts removed")
-    post.save().then(data => console.log("Post saved"))
-        .catch(err => console.log("There was an error", err))  
-})
+const loadShit = async function() {
+    const p1 = Schemas.User.deleteMany({}, err => {
+        console.log("Users removed")
+        user.save().then(() => console.log("User saved"))
+            .catch(err => console.log("There was an error", err))    
+        user2.save().then(() => console.log("User saved"))
+            .catch(err => console.log("There was an error", err))    
+        
+    })
+    const p2 = Schemas.Post.deleteMany({}, err => {
+        console.log("Posts removed")
+        post.save().then(data => console.log("Post saved"))
+            .catch(err => console.log("There was an error", err))  
+    })
+
+    await Promise.all([p1,p2])
+    await mongoose.disconnect()
+}
+
+
+loadShit()
 
 
 
