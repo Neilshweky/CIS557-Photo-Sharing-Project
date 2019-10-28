2c59ed5031ad3e1198b16c82e3b5063f36d77510
Carlos Bros Gallego <cbros@sas.upenn.edu>
2019-10-27 22:48:30 -0400

    Added tests for createPost and add/getFriend

--- api/models/database.test.js
+++ api/models/database.test.js
@@ -3,20 +3,21 @@ const mongoose = require('mongoose');
 const Schemas = require('./schemas');
 
 beforeAll(async done => {
-    await mongoose.disconnect()
+    await mongoose.disconnect();
     await mongoose.connect('mongodb://localhost/cis557_db_test', {
         useNewUrlParser: true,
         useUnifiedTopology: true
       });
-    done()
-})
+    done();
+});
+
 describe('authentication tests', () => {
 
     beforeEach(async () => {
         await Schemas.User.deleteMany({});
-    })
+    });
 
-    test ('create user test', async () => {
+    test ('createUser test', async () => {
         var user = await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "cis557", "some_pic");
         expect(user.username).toEqual("neilshweky");
         expect(user.email).toEqual("nshweky@seas.upenn.edu");
@@ -24,41 +25,80 @@ describe('authentication tests', () => {
         expect(user.profile_picture).toEqual("some_pic");
         expect(Array.from(user.friends)).toEqual([]);
         expect(Array.from(user.posts)).toEqual([]);
-    })
+    });
 
-    test ('create user duplicate', async () => {
+    test ('createUser duplicate', async () => {
         await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "cis557", "some_pic");
         var user2 = await db.createUser("neilshweky", "nshweky2@seas.upenn.edu", "cis557", "some_pic");
         expect(user2).toEqual(undefined);
-    })
+    });
 
-    test ('login successful', async () => {
+    test ('checkLogin successful', async () => {
         var user = await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "cis557", "some_pic");
         var result = await db.checkLogin("neilshweky", "cis557");
         expect(result.username).toEqual(user.username);
         expect(result.password).toEqual(user.password);
         expect(result.email).toEqual(user.email);
         expect(result.profile_picture).toEqual(user.profile_picture);
-    })
+    });
 
-    test ('login not successful', async () => {
+    test ('checkLogin not successful', async () => {
         var user = await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "cis557", "some_pic");
         var result = await db.checkLogin("neilshweky", "cis557-2");
         expect(result).toEqual(null);
-    })
+    });
+});
 
-    test ('login throws error', async () => {
-        var user = await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "cis557", "some_pic");
-        // var result = await db.login("neilshweky", "cis557sucks2")
-        // expect(result).toEqual([])
-        // HOW TO WRITE THIS???
-    })
+describe('friend tests', () => {
 
-})
+  beforeEach (async () => {
+    await Schemas.User.deleteMany({});
+    await db.createUser("user1", "user1@seas.upenn.edu", "pw-1", "pic1");
+    await db.createUser("user2", "user2@seas.upenn.edu", "pw-2", "pic2");
+    await db.createUser("user3", "user3@seas.upenn.edu", "pw-3", "pic3");
+  });
 
+  test ('addFriend and getFriend test', async () => {
+    await db.addFriend("user1", "user2");
+    await db.addFriend("user1", "user3");
+    await db.addFriend("user2", "user3");
+    var user1Friends = await db.getFriendsForUsername("user1");
+    expect(user1Friends[0]).toEqual("user2");
+    expect(user1Friends[1]).toEqual("user3");
+    var user2Friends = await db.getFriendsForUsername("user2");
+    expect(user2Friends[0]).toEqual("user3");
+  });
+});
 
+describe('post tests', () => {
+
+  beforeEach(async () => {
+    await Schemas.Post.deleteMany({});
+    await db.createUser("cbros", "cbros@seas.upenn.edu", "pw-1", "pic1");
+    await db.createUser("neilshweky", "nshweky@seas.upenn.edu", "pw-2", "pic2");
+    await db.addFriend("cbros", "neilshweky");
+  });
+
+  test ('createPost test', async () => {
+    var post = await db.createPost("some_pic", "cbros");
+    expect(post.username).toEqual("cbros");
+    expect(post.picture).toEqual("some_pic");
+    expect(Array.from(post.likes)).toEqual([]);
+    expect(Array.from(post.comments)).toEqual([]);
+  });
+
+  test ('postPicture test', async () => {
+    var post = await db.postPicture("picture", "cbros");
+    expect(post.username).toEqual("cbros");
+    expect(post.picture).toEqual("picture");
+    // Check that post appears in friend's feed
+    var friend = await db.getUser("neilshweky");
+    expect(friend.posts[0]).toEqual(post.uid);
+  });
+
+});
 
 afterAll(async done => {
-    mongoose.disconnect()
-    done()
-})
+    mongoose.disconnect();
+    done();
+});
