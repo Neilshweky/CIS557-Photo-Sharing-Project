a56ca9986ca1937138ad9e94b3ce9f375240206b
Neil Shweky <neilshweky@gmail.com>
2019-12-02 20:22:32 -0500

    serve

--- client/server.js
+++ client/server.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + '/build'));
+const port = process.env.PORT || '3000';
+app.listen(port);
+console.log(`Server running on port ${port}. Now open http://localhost:${port}/ in your browser!`);
